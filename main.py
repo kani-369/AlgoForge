bf0e46190c9e7ca2bd6b30711ba1a5d0b505f321
kani-369 <flexocave123@gmail.com>
2025-10-30 19:43:19 +0530

    skeleton main file

---
+++ main.py
@@ -0,0 +1,11 @@
+from utils.benchmark import benchmark
+from utils.parser import parse_input_parser
+
+def main():
+    print("Algoforge --Started--")
+
+
+
+
+if __name__ == "__main__":
+    main()
